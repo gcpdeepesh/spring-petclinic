43983d1b33150bbbe4ed9e591881fd26b43943e7
gcpdeepesh <gcpdeepesh@gmail.com>
2020-06-13 11:24:39 +0000

    Updated appspec.yml to STDOUT to dev-null

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-nohup java -jar /opt/app/spring-petclinic-2.3.0.BUILD-SNAPSHOT.jar &
+echo 'Killing if existing process'
+killall -9 java
+echo 'Starting application at: ' `date`
+java -jar /opt/app/spring-petclinic-2.3.0.BUILD-SNAPSHOT.jar > /dev/null 2>&1 &
